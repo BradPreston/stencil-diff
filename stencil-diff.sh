b354373dbd9c2e5bed98472df71c21431da9a1c2
bradpreston <bap5393@gmail.com>
2024-02-17 10:51:30 -0500

    Comment out error checks in shell file

--- stencil-diff.sh
+++ stencil-diff.sh
@@ -4,14 +4,14 @@
 stencilFolder=$1
 gitFolder=$2
 
-if [ ! -d "$stencilFolder" ] ; then
-  echo "Error: Stencil theme not found"
-  exit
-fi
+# if [ ! -d "$stencilFolder" ] ; then
+#   echo "Error: Stencil theme not found"
+#   exit
+# fi
 
-if [ ! -d "$gitFolder" ] ; then
-  echo "Error: Git repo theme not found"
-  exit
-fi
+# if [ ! -d "$gitFolder" ] ; then
+#   echo "Error: Git repo theme not found"
+#   exit
+# fi
 
 diff -qr --exclude=node_modules --exclude=dist --exclude=.DS_Store --exclude=parsed --exclude=package-lock.json --exclude=manifest.json --exclude=.git $stencilFolder $gitFolder
